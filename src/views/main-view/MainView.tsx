0dddb3ca7cc325e161f454f69d1d1cba823b890d
szczypiorofix <szczypiorofix@o2.pl>
2023-11-06 21:00:39 +0100

    Add style to MainView

--- src/views/main-view/MainView.tsx
+++ src/views/main-view/MainView.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import "./MainView.scss";
+
 export const MainView: React.FC = () => {
   return (
-    <div>
-      <h1>MAIN VIEW</h1>
-    </div>
+      <div className="main-view-background">
+          <div className="container main-view">
+              <h1 className="title">MAIN VIEW</h1>
+          </div>
+      </div>
   );
 };

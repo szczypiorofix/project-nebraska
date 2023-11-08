70245da67fd5ba1c5439d36e7577d919b9c0018c
szczypiorofix <szczypiorofix@o2.pl>
2023-11-08 23:45:48 +0100

    Add useLocalStorage error resolving

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,17 +1,49 @@
 import { useState } from 'react';
 
-export function useLocalStorage<T>( defaultValue: T, objectName: string ): [ T, ( saveObj: T) => void, () => void, () => void ] {
-    const [ state, setState ] = useState<T>( defaultValue );
-    const save = ( obj: T ): void => {
-        localStorage.setItem( objectName, JSON.stringify( obj ) );
+export interface LocalStorageError {
+    code: number;
+    error: boolean;
+    message: string;
+}
+
+export function useLocalStorage<T>(defaultValue: T, objectName: string): [T, (saveObj: T) => void, () => void, LocalStorageError, () => void] {
+    const [state, setState] = useState<T>(defaultValue);
+    const [error, setError] = useState<LocalStorageError>({
+        code: 0,
+        error: false,
+        message: ""
+    });
+    const save = (obj: T): void => {
+        let objectAsString: string = "{}";
+        try {
+            objectAsString = JSON.stringify(obj);
+        } catch(err) {
+            console.error(err);
+            setError({
+                code: 1,
+                error: true,
+                message: JSON.stringify(err)
+            });
+        }
+        localStorage.setItem(objectName, objectAsString);
     }
     const load = (): void => {
-        const objStr: string = localStorage.getItem( objectName ) ?? "";
-        const obj: T = ( JSON.parse( objStr ) ?? defaultValue ) as T;
-        setState( obj );
+        const objStr: string = localStorage.getItem(objectName) ?? "";
+        let obj: T = defaultValue;
+        try {
+            obj = JSON.parse(objStr);
+        } catch(err) {
+            console.error(err);
+            setError({
+                code: 2,
+                error: true,
+                message: JSON.stringify(err)
+            });
+        }
+        setState(obj);
     }
     const debug = (): void => {
-        console.dir( state );
+        console.dir(state);
     }
-    return [ state, save, load, debug ];
+    return [state, save, load, error, debug];
 }

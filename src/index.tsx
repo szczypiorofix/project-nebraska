292daad366d6bc88bab97b3a790d29aeb7bb64be
szczypiorofix <szczypiorofix@o2.pl>
2024-01-28 23:49:41 +0100

    Downgrade React from 18 to 17.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 
 import App from './App';
 
 import "./index.scss";
 
-const root: ReactDOM.Root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
-);
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+ReactDOM.render(
+    <React.StrictMode><App /></React.StrictMode>,
+    document.getElementById("root")
 );
